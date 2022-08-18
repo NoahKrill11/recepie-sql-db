ab320a7d22c3cb9ec03150fa2f4dc2d307f81891
Noah Krill <51893284+NoahKrill11@users.noreply.github.com>
2022-08-17 21:41:29 -0400

    ADD UOM TABLE AND RECEPIE TABLE

--- create_tables.sql
+++ create_tables.sql
@@ -1,6 +1,30 @@
 use recepieBook
+go
+
+Drop table if exists recepieBook.recepie;
+go
+
+create table ingrediants
+(
+	ingredeintID int not null PRIMARY KEY,
+	ingrediantName varchar(255)
+);
+
+go 
+
+create table UOM
+(
+	UOMID int not null PRIMARY KEY,
+	UOM varchar(255)
+);
 
 create table recepie
 (
-	recepieName varchar(255)
+	recepieName varchar(255),
+	author varchar(255),
+	createdDate datetime,
+	ingredeintID int FOREIGN KEY references ingrediants (ingredeintID),
+	quantity float(25),
+	UOMID int FOREIGN KEY references UOM (UOMID)
+
 );
